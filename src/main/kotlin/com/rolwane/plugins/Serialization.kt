fedf50010d99db6e8d138059038757791b5a87b7
rolwane <rolwane@hotmail.com>
2022-12-19 15:06:30 -0300

    initial commit

---
+++ src/main/kotlin/com/rolwane/plugins/Serialization.kt
@@ -0,0 +1,19 @@
+package com.rolwane.plugins
+
+import io.ktor.serialization.jackson.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.server.plugins.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+    routing {
+    }
+}
