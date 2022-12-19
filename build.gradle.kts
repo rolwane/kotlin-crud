d4901cdd1053c6aac1835f217bc9d4843747b004
rolwane <rolwane@hotmail.com>
2022-12-19 16:14:05 -0300

    added mysql connector dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("org.ktorm:ktorm-core:${ktorm_version}")
+    implementation("mysql:mysql-connector-java:5.1.13")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
